cda900f75c8e55ff2054383dde8f1abb1c485a7b
EshanAdithaya <87906517+EshanAdithaya@users.noreply.github.com>
2024-06-28 11:49:11 +0530

    updates stable

--- app/src/main/java/com/example/cryptoapp/MainActivity.java
+++ app/src/main/java/com/example/cryptoapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -13,7 +14,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -40,13 +40,10 @@ public class MainActivity extends AppCompatActivity {
 
     private ImageView userImageView;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
 
         // Find the button by its ID
         AppCompatButton cryptoValueButton = findViewById(R.id.cryptoValue);
@@ -59,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
         // Initialize Firebase
         mAuth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
@@ -79,30 +77,50 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.view);
         recyclerView.setLayoutManager(gridLayoutManager);
 
-        // Uncomment and complete the following lines if you have the necessary classes
-         ArrayList<CryptoWallet> cryptoWalletArrayList = new ArrayList<>();
-         cryptoWalletArrayList.add(new CryptoWallet("BTC", "btc", 2.13, 1.4, 14021.35));
-         cryptoWalletArrayList.add(new CryptoWallet("ETH", "eth", -1.13, 3.6, 2145.21));
-         cryptoWalletArrayList.add(new CryptoWallet("XRP", "xrp", -3.14, 2.6, 21463.10));
-         cryptoWalletArrayList.add(new CryptoWallet("LTC", "ltc", 4.54, 3.5, 5412.46));
-         adapter = new RecyclerView.Adapter() {
-             @NonNull
-             @Override
-             public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                 return null;
-             }
+        // Sample data
+        ArrayList<CryptoWallet> cryptoWalletArrayList = new ArrayList<>();
+        cryptoWalletArrayList.add(new CryptoWallet("BTC", "btc", 2.13, 1.4, 14021.35));
+        cryptoWalletArrayList.add(new CryptoWallet("ETH", "eth", -1.13, 3.6, 2145.21));
+        cryptoWalletArrayList.add(new CryptoWallet("XRP", "xrp", -3.14, 2.6, 21463.10));
+        cryptoWalletArrayList.add(new CryptoWallet("LTC", "ltc", 4.54, 3.5, 5412.46));
+        cryptoWalletArrayList.add(new CryptoWallet("ADA", "ada", 1.23, 2.8, 1.34));
+        cryptoWalletArrayList.add(new CryptoWallet("DOT", "dot", -0.75, 4.1, 15.72));
+        cryptoWalletArrayList.add(new CryptoWallet("LINK", "link", 2.45, 1.9, 18.36));
+        cryptoWalletArrayList.add(new CryptoWallet("BNB", "bnb", 3.67, 3.2, 324.57));
+        cryptoWalletArrayList.add(new CryptoWallet("DOGE", "doge", -2.56, 2.5, 0.255));
+        cryptoWalletArrayList.add(new CryptoWallet("SOL", "sol", 5.89, 4.5, 32.18));
+        cryptoWalletArrayList.add(new CryptoWallet("UNI", "uni", 0.92, 2.3, 19.84));
+        cryptoWalletArrayList.add(new CryptoWallet("MATIC", "matic", 3.14, 3.8, 1.25));
+        cryptoWalletArrayList.add(new CryptoWallet("ICP", "icp", -4.32, 2.1, 34.76));
+        cryptoWalletArrayList.add(new CryptoWallet("VET", "vet", 1.87, 1.7, 0.127));
+        cryptoWalletArrayList.add(new CryptoWallet("EOS", "eos", 0.51, 2.4, 3.89));
+        cryptoWalletArrayList.add(new CryptoWallet("XLM", "xlm", -1.05, 2.9, 0.365));
+        cryptoWalletArrayList.add(new CryptoWallet("TRX", "trx", 1.98, 2.2, 0.078));
+        cryptoWalletArrayList.add(new CryptoWallet("ATOM", "atom", 2.76, 3.3, 8.94));
+        cryptoWalletArrayList.add(new CryptoWallet("XTZ", "xtz", 0.83, 2.6, 2.97));
+        cryptoWalletArrayList.add(new CryptoWallet("AAVE", "aave", 3.82, 3.7, 215.63));
+        cryptoWalletArrayList.add(new CryptoWallet("CAKE", "cake", 5.67, 4.2, 13.84));
+        cryptoWalletArrayList.add(new CryptoWallet("ALGO", "algo", 0.92, 2.1, 0.88));
+        cryptoWalletArrayList.add(new CryptoWallet("FTT", "ftt", 4.56, 3.9, 28.74));
+        cryptoWalletArrayList.add(new CryptoWallet("FIL", "fil", -2.34, 2.3, 51.32));
+        cryptoWalletArrayList.add(new CryptoWallet("AVAX", "avax", 6.45, 4.5, 11.87));
+        cryptoWalletArrayList.add(new CryptoWallet("ZEC", "zec", -1.78, 2.4, 108.29));
+        cryptoWalletArrayList.add(new CryptoWallet("HBAR", "hbar", 1.34, 2.2, 0.183));
+        cryptoWalletArrayList.add(new CryptoWallet("WAVES", "waves", 2.91, 3.1, 13.56));
+        cryptoWalletArrayList.add(new CryptoWallet("NEO", "neo", 0.76, 2.0, 38.29));
+        cryptoWalletArrayList.add(new CryptoWallet("DASH", "dash", 0.45, 2.5, 157.84));
+        cryptoWalletArrayList.add(new CryptoWallet("XMR", "xmr", -0.92, 2.8, 232.17));
+        cryptoWalletArrayList.add(new CryptoWallet("CRO", "cro", 1.87, 2.6, 0.135));
+        cryptoWalletArrayList.add(new CryptoWallet("KSM", "ksm", 3.45, 3.4, 314.52));
+        cryptoWalletArrayList.add(new CryptoWallet("GRT", "grt", 2.15, 2.7, 0.654));
+        cryptoWalletArrayList.add(new CryptoWallet("IOST", "iost", 0.63, 2.3, 0.017));
+        cryptoWalletArrayList.add(new CryptoWallet("REN", "ren", -1.29, 2.9, 0.345));
+        cryptoWalletArrayList.add(new CryptoWallet("RUNE", "rune", 4.87, 3.8, 4.32));
+        cryptoWalletArrayList.add(new CryptoWallet("UMA", "uma", 5.32, 4.0, 10.78));
+        cryptoWalletArrayList.add(new CryptoWallet("LUNA", "luna", 6.54, 4.3, 6.92));
 
-             @Override
-             public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
-             }
-
-             @Override
-             public int getItemCount() {
-                 return 0;
-             }
-         };
-         recyclerView.setAdapter(adapter);
+        adapter = new CryptoWalletAdapter(cryptoWalletArrayList);
+        recyclerView.setAdapter(adapter);
     }
 
     private void setupProfileIconClick() {
@@ -143,7 +161,6 @@ public class MainActivity extends AppCompatActivity {
                 .show();
     }
 
-
     private void logoutCurrentUser() {
         mAuth.signOut();
     }
@@ -153,6 +170,7 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
+
     private void fetchUserData() {
         if (currentUser != null) {
             String uid = currentUser.getUid();
@@ -173,11 +191,11 @@ public class MainActivity extends AppCompatActivity {
                             userEmailTextView.setText(userEmail != null ? userEmail : "No email");
 
                             // Load profile image using Glide
-                            if (profileImageURL!=null) {
+                            if (profileImageURL != null) {
                                 Glide.with(MainActivity.this)
                                         .load(profileImageURL)
-//                                        .placeholder(R.drawable.baseline_person_24)
-//                                        .error(R.drawable.baseline_person_24)
+                                        .placeholder(R.drawable.baseline_person_24)
+                                        .error(R.drawable.baseline_person_24)
                                         .into(userImageView);
                             } else {
                                 userImageView.setImageResource(R.drawable.baseline_person_24);
@@ -188,19 +206,102 @@ public class MainActivity extends AppCompatActivity {
                             userNameTextView.setText("No user data");
                             userEmailTextView.setText("No user data");
                         }
-
                     });
         }
     }
-
-
-
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         if (userListener != null) {
             userListener.remove();
+        }
+    }
+
+    // Sample CryptoWallet class
+    class CryptoWallet {
+        String name;
+        String symbol;
+        double change;
+        double volume;
+        double price;
+
+        public CryptoWallet(String name, String symbol, double change, double volume, double price) {
+            this.name = name;
+            this.symbol = symbol;
+            this.change = change;
+            this.volume = volume;
+            this.price = price;
+        }
+
+        // Getters
+        public String getName() {
+            return name;
+        }
+
+        public String getSymbol() {
+            return symbol;
+        }
+
+        public double getChange() {
+            return change;
+        }
+
+        public double getVolume() {
+            return volume;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+    }
+
+    // Adapter for RecyclerView
+    class CryptoWalletAdapter extends RecyclerView.Adapter<CryptoWalletAdapter.ViewHolder> {
+        private ArrayList<CryptoWallet> cryptoWalletList;
+
+        CryptoWalletAdapter(ArrayList<CryptoWallet> cryptoWalletList) {
+            this.cryptoWalletList = cryptoWalletList;
+        }
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.crypto_item, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            CryptoWallet currentItem = cryptoWalletList.get(position);
+
+            holder.nameTextView.setText(currentItem.getName());
+            holder.symbolTextView.setText(currentItem.getSymbol());
+            holder.changeTextView.setText(String.valueOf(currentItem.getChange()));
+            holder.volumeTextView.setText(String.valueOf(currentItem.getVolume()));
+            holder.priceTextView.setText(String.valueOf(currentItem.getPrice()));
+        }
+
+        @Override
+        public int getItemCount() {
+            return cryptoWalletList.size();
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+            TextView nameTextView;
+            TextView symbolTextView;
+            TextView changeTextView;
+            TextView volumeTextView;
+            TextView priceTextView;
+
+            ViewHolder(View itemView) {
+                super(itemView);
+                nameTextView = itemView.findViewById(R.id.crypto_name);
+                symbolTextView = itemView.findViewById(R.id.crypto_symbol);
+                changeTextView = itemView.findViewById(R.id.crypto_change);
+                volumeTextView = itemView.findViewById(R.id.crypto_volume);
+                priceTextView = itemView.findViewById(R.id.crypto_price);
+            }
         }
     }
 }
